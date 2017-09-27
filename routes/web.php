cbe5290ca4deb304982baacaaa619fb2c74bebd4
a0952288480 <a0952288480@student.ncut.edu.tw>
2017-09-27 11:34:32 +0800

    建立路由

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,19 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('tasks');
+});
+
+/**
+ * 增加新的任務
+ */
+Route::post('/task', function (Request $request) {
+    //
+});
+
+/**
+ * 刪除任務
+ */
+Route::delete('/task/{task}', function (Task $task) {
+    //
 });
